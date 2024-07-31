e09d80580a12ca73f36e28ac264e69c051cc051b
Natalia Kubiak <nataliia.kubiak@gmail.com>
2024-07-31 18:57:55 +0200

    add handling 2 clients in different threads

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -1,46 +1,35 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.time.LocalDateTime;
 
 public class ChatServer {
     public static final int PORT = 8060;
 
     public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
 
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+        try {
+            serverSocket = new ServerSocket(PORT);
             System.out.println("Сервер стартовал");
-            while (true) {
-                try (Socket clientSocket = serverSocket.accept(); //waiting client to connect
-                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-                ) {
-                    System.out.println("New connection accepted");
-                    //in - what we get from client
-                    final String name = in.readLine();
-                    LocalDateTime time = LocalDateTime.now();
-                    System.out.printf("[%s] Client name: %s\n", time, name);
 
-                    boolean flag = true;
-                    while (flag) {
-                        String msg = in.readLine();
-                        if ("exit".equals(msg)) {
-                            System.out.printf("[%s] %s quit this amazing chat (what a fool!)\n", time, name);
-                            flag = false;
-                        } else {
-                            System.out.printf("[%s] %s: %s\n", time, name, msg);
-                            //out - what we send to client
-                            out.println(String.format("[%s] %s: %s", time, name, msg));
-                        }
-                    }
-                }
+            while (true) {
+                    clientSocket = serverSocket.accept();
+                    ClientHandler clientHandler = new ClientHandler(clientSocket);
+                    new Thread(clientHandler).start();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 }
---
+++ src/main/java/server/ClientHandler.java
@@ -0,0 +1,64 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.time.LocalDateTime;
+
+public class ClientHandler implements Runnable {
+
+    private Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("New connection accepted on port " + clientSocket.getPort());
+            //in - what we get from client
+            final String name = in.readLine();
+            LocalDateTime time = LocalDateTime.now();
+            System.out.printf("[%s] Client name: %s\n", time, name);
+
+            boolean flag = true;
+            while (flag) {
+                String msg = in.readLine();
+                if ("exit".equals(msg)) {
+                    System.out.printf("[%s] %s quit this amazing chat (what a fool!)\n", time, name);
+                    flag = false;
+                } else {
+                    System.out.printf("[%s] %s: %s\n", time, name, msg);
+                    //out - what we send to client
+                    out.println(String.format("[%s] %s: %s", time, name, msg));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+                System.out.println(">>>>> ClientSocket on port " + clientSocket.getPort() + " closed");
+                if (in != null) {
+                    in.close();
+                    System.out.println(">>>>> PORT " + clientSocket.getPort() + " INPUT Stream closed");
+                }
+                if (out != null) {
+                    out.close();
+                    System.out.println(">>>>> PORT " + clientSocket.getPort() + " OUTPUT Stream closed");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
